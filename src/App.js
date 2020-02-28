dba6ecc4d95b3872c7dc4823698fc7408e6ff0d0
PajkoStefan <52094853+PajkoStefan@users.noreply.github.com>
2020-02-28 20:40:52 +0100

    CSS Modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import './App.css';
+import classes from './App.module.css';
 // import Validation from "./components/assignment-2/Validation";
 // import Char from "./components/assignment-2/Char";
 import Person from "./components/Person/Person";
@@ -65,6 +65,7 @@ class App extends Component {
 
     render() {
         let persons = null;
+        let btnClass = ''; //button class
 
         if (this.state.showPersons) {
             persons = (
@@ -86,25 +87,29 @@ class App extends Component {
                 //     backgroundColor: 'salmon',
                 //     color: 'black'
                 // };
+            // change btnClass
+            btnClass = classes.Red;
+            console.log(btnClass);
         }
 
-        const classes = [];
+
+        const assignedClasses = [];
         if (this.state.persons.length <= 2) {
-            classes.push('red'); // classes = ['red']
+            assignedClasses.push(classes.red); // classes = ['red']
         }
         if (this.state.persons.length <= 1) {
-            classes.push('bold'); // classes = ['red', 'bold']
+            assignedClasses.push(classes.bold); // classes = ['red', 'bold']
         }
 
         return (
-            <div className="App">
+            <div className={classes.App}>
                 <h1>Hi, I'm a React App</h1>
-                <p className={classes.join(' ')}>This is really working!</p>
-                <StyledButton
-                    alt={this.state.showPersons}
+                <p className={assignedClasses.join(' ')}>This is really working!</p>
+                <button
+                    className={btnClass}
                     onClick={this.togglePersonsHandler}>
                     Toggle Persons
-                </StyledButton>
+                </button>
                 {persons}
             </div>
         );
