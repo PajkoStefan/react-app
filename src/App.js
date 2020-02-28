44a22845c3658973eeeb7403148eae59686c1b7d
PajkoStefan <52094853+PajkoStefan@users.noreply.github.com>
2020-02-28 17:55:23 +0100

    using Radim for Media Queries

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 // import Char from "./components/assignment-2/Char";
 import Person from "./components/Person/Person";
 import Radium from "radium";
+import StyleRoot from "radium/es/components/style-root";
 
 class App extends Component {
 
@@ -95,16 +96,18 @@ class App extends Component {
         }
 
         return (
-            <div className="App">
-                <h1>Hi, I'm a React App</h1>
-                <p className={classes.join(' ')}>This is really working!</p>
-                <button
-                    style={style}
-                    onClick={this.togglePersonsHandler}>
-                    Toggle Persons
-                </button>
-                {persons}
-            </div>
+            <StyleRoot>
+                <div className="App">
+                    <h1>Hi, I'm a React App</h1>
+                    <p className={classes.join(' ')}>This is really working!</p>
+                    <button
+                        style={style}
+                        onClick={this.togglePersonsHandler}>
+                        Toggle Persons
+                    </button>
+                    {persons}
+                </div>
+            </StyleRoot>
         );
 
     }
--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -3,8 +3,14 @@ import './style.css';
 import Radium from 'radium';
 
 const Person = (props) => {
+    const style = {
+        '@media(min-width: 500px)':{
+            width: '450px'
+        }
+    };
+
     return (
-        <div className="person">
+        <div className="person" style={style}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
